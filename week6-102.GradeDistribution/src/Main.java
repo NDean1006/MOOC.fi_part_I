2a72bad409574aaff3c5943b09bce1042654dada
Ndean85 <Ndean85@gmail.com>
2017-05-08 14:31:04 -0400

    Completed

--- week6-102.GradeDistribution/src/Main.java
+++ week6-102.GradeDistribution/src/Main.java
@@ -2,11 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner lukija = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
         // implement your program here
         // do not put all to one method/class but rather design a proper structure to your program
         
         // Your program should use only one Scanner object, i.e., it is allowed to call 
         // new Scanner only once. If you need scanner in multiple places, you can pass it as parameter
+        System.out.println("Type exam scores, -1 completes:");
+
+        Stars stars = new Stars();
+
+        stars.doSomething(reader);
+        stars.printStars();
+        stars.printAcceptance();
     }
 }
+    
+
